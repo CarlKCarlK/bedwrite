8c46611c622324c82bf7a595faa9725e905133ff
Carl Kadie <carlk@msn.com>
2022-01-23 14:40:39 -0800

    checkpoint

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
-use dpc_pariter::IteratorExt;
+use dpc_pariter::{scope, IteratorExt};
 use ndarray as nd;
 
 use std::{
-    fs::File,
+    fs::{self, File},
     io::{BufWriter, Write},
 };
 
@@ -10,84 +10,70 @@ use thiserror::Error;
 
 pub fn write(
     filename: &str,
-    iid_count: usize,
-    sid_count: usize,
-    high: f64,
+    iid_count: usize, //e.g. 5000, 50_000, 500_000
+    sid_count: usize, //e.g. 5 to 50_000 (but only to 5K when iid is 500K)
+    high: i8,
+    num_threads: usize,
 ) -> Result<(), BedError> {
     assert!(iid_count % 4 == 0, "iid_count must be a multiple of 4");
     let iid_count_div4 = iid_count / 4;
-    let val = nd::Array::from_elem((iid_count, sid_count), high - 0.01);
+    let val: nd::Array2<i8> = nd::Array::from_elem((iid_count, sid_count), high);
 
-    let mut writer = BufWriter::new(File::create(filename)?);
-
-    val.axis_iter(nd::Axis(1))
-        .parallel_map(|column| {
-            // Covert each column into a bytes_vector
-            let mut bytes_vector: Vec<u8> = vec![0; iid_count_div4]; // inits to 0
-            for (iid_i, &v0) in column.iter().enumerate() {
-                let byte = (v0 / 4.0f64).floor() as u8;
-                let i_div_4 = iid_i / 4;
-                let i_mod_4 = iid_i % 4;
-                bytes_vector[i_div_4] |= byte << (i_mod_4 * 2);
-            }
-            bytes_vector
-        })
-        .for_each(|bytes_vector| {
-            writer.write_all(&bytes_vector).unwrap();
-        });
-    return Ok(());
+    if let Err(e) = write_internal(filename, iid_count_div4, &val, num_threads) {
+        // Clean up the file
+        let _ = fs::remove_file(filename);
+        Err(e)
+    } else {
+        Ok(())
+    }
 }
 
-// val.axis_iter(nd::Axis(1))
-//     .parallel_map(|column| {
-//         // Covert each column into a bytes_vector
-//         let mut bytes_vector: Vec<u8> = vec![0; iid_count_div4]; // inits to 0
-//         for (iid_i, &v0) in column.iter().enumerate() {
-//             let byte = if v0 < 4.0 {
-//                 (v0 / 4.0f64).floor() as u8
-//             } else {
-//                 return Err(BedError::BadValue(filename.to_string()).into());
-//             };
-//             let i_div_4 = iid_i / 4;
-//             let i_mod_4 = iid_i % 4;
-//             bytes_vector[i_div_4] |= byte << (i_mod_4 * 2);
-//         }
-//         Ok(bytes_vector)
-//     })
-//     .try_for_each(|result: Result<Vec<u8>, BedError>| match result {
-//         Ok(bytes_vector) => {
-//             for byte in bytes_vector {
-//                 writer.write_all(&[byte])?;
-//             }
-//             Ok(())
-//         }
-//         Err(e) => Err(e),
-//     })?;
-// .try_for_each(|result| match result {
-//     Ok(bytes_vector) => {
-//         writer.write_all(&bytes_vector)?;
-//     }
-//     Err(e) => return Err(e),
-// });
+fn write_internal(
+    filename: &str,
+    iid_count_div4: usize,
+    val: &nd::Array2<i8>,
+    num_threads: usize,
+) -> Result<(), BedError> {
+    let mut writer = BufWriter::new(File::create(filename)?);
 
-// for column in val.axis_iter(nd::Axis(1)) {
-//     // Covert each column into a bytes_vector
-//     let mut bytes_vector: Vec<u8> = vec![0; iid_count_div4]; // inits to 0
-//     for (iid_i, &v0) in column.iter().enumerate() {
-//         let byte = if v0 < 4.0 {
-//             (v0 / 4.0f64).floor() as u8
-//         } else {
-//             return Err(BedError::BadValue(filename.to_string()).into());
-//         };
-//         let i_div_4 = iid_i / 4;
-//         let i_mod_4 = iid_i % 4;
-//         bytes_vector[i_div_4] |= byte << (i_mod_4 * 2);
-//     }
-//     // Write the bytes vector, they must be in order.
-//     writer.write_all(&bytes_vector)?;
-// }
-// return Ok(());
-//}
+    let result_list: Result<Vec<()>, BedError> = 
+        // 'scope' checks that all threads finish before returning
+        //cmk scope(|scope| {
+        // for each column in the array
+        val.axis_iter(nd::Axis(1))
+            // '.parallel_map_scoped' works with 'scope' to avoid
+            // a 'borrowed value does not live long enough' error.
+            .parallel_map({ ///cmk _scoped(scope, {
+                move |column| {
+                    // Convert each column into a bytes_vector
+                    let mut bytes_vector: Vec<u8> = vec![0; iid_count_div4]; // inits to 0
+                    for (iid_i, &v0) in column.iter().enumerate() {
+                        if v0 > 3 {
+                            return Err(BedError::BadValue(filename.to_string()));
+                        }
+                        let byte = v0 as u8;
+                        let i_div_4 = iid_i / 4;
+                        let i_mod_4 = iid_i % 4;
+                        bytes_vector[i_div_4] |= byte << (i_mod_4 * 2);
+                    }
+                    Ok(bytes_vector)
+                }
+            })
+            .threads(num_threads)
+            .map(|bytes_vector: Result<_, BedError>| {
+                // Write the bytes vector, they must be in order.
+                writer.write_all(&bytes_vector?)?;
+                Ok(())
+            }
+            .collect();
+    //     )
+    // })
+    // .map_err(|_e| BedError::PanickedThread())?;
+
+    result_list?;
+
+    Ok(())
+}
 
 #[derive(Error, Debug)]
 pub enum BedError {
@@ -96,21 +82,30 @@ pub enum BedError {
 
     #[error(transparent)]
     IOError(#[from] std::io::Error),
+
+    #[error("Multithreading resulted in panic(s)")]
+    PanickedThread(),
 }
 
 #[test]
 fn test1() {
-    write("test1.bed", 12, 10, 4.0).unwrap();
+    write("test1.bed", 12, 10, 3, 12).unwrap();
 }
 
 #[test]
 fn test2() {
-    let result = write("test2.bed", 12, 10, 5.0);
+    let result = write("test2.bed", 12, 10, 4, 1);
     assert!(result.is_err());
 }
 
 #[test]
-fn big() {
+fn big1() {
     // too slow:
-    write("big.bed", 1_000_000, 100, 4.0).unwrap();
+    write("big1.bed", 50_000, 5000, 2, 1).unwrap();
+}
+
+#[test]
+fn big12() {
+    // too slow:
+    write("big12.bed", 50_000, 5000, 2, 12).unwrap();
 }
